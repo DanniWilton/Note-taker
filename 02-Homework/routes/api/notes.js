86f93392bc225068116f9412c288a6c3dd8e18b5
Danni Wilton <danniwilton@gmail.com>
2021-07-09 14:01:06 +0800

    changed server.js & added route files

---
+++ 02-Homework/routes/api/notes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const path = require("path");
+const uuid = require("uuid");
+const fs = require("fs");
+
+const dbPath = __dirname + "/../../Develop/db/db.json";
+
+const db = require(dbPath);
+
+
+
+router.get('/api/notes', (req, res) => {
+    // get a list of notes from db
+
+    return res.json(db);
+    
+});
+
+router.post('/api/notes', (req, res) => {
+    // add a note to db
+
+    // get the title and body payload from user
+    const title = req.body.title;
+    const text = req.body.text;
+    console.log(title, text);
+
+    // save it to the array
+    db.push({
+        text,
+        title,
+        id: uuid.v4(), 
+    });
+
+    // save it to the db.json file
+    fs.writeFileSync(dbPath, JSON.stringify(db));
+
+    // send a res back
+    res.json({
+        data: "success"
+    })
+
+
+});
+
+
+module.exports = router;
--- 02-Homework/server.js
+++ 02-Homework/server.js
@@ -2,48 +2,21 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+
+const webRoutes = require("./routes/web/web");
+const notesRoutes = require("./routes/api/notes");
+
 let db = require('./Develop/db/db.json');
-const { request } = require("http");
-const { response } = require("express");
 
-const PORT = process.env.PORT || 8080;
 
-// set up express
+app.use(express.static(path.join(__dirname, 'Develop', "public")));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// routes
-// static files
-app.use(express.static("./public"));
-// get notes.html
-app.get('/notes', (request, response) => {
-    response.sendFile(path.join(__dirname, './public/notes.html'));
-});
-// function that retrieves the db.json file
-app.get('/api/notes', (request, response) => response.sendFile(path.join(__dirname, "./db/db.json")));
+app.use(webRoutes);
+app.use(notesRoutes);
 
+const PORT = 8080
 
-app.get('/api/notes', (request, response) => 
-    fs.readFile("./db/db.json", (error, jsonString) => {
-        if (error) {
-            console.log("fail", error);
-            return;
-        }
-        response.json(JSON.parse(jsonString));
-    }))
-
-app.post('/api/notes', (request, response) => {
-    const note = {
-        id: shortid.generate(),
-        title: req.body.title,
-        text: req.body.text
-    }
-    db.push(note)
-    fs.writeFile('./db/db.json', JSON.stringify(db), err => {
-        err ? console.log(err): console.log("it worked!")
-        res.json(db)
-    })
-})
 
 
 app.listen(PORT, () => {
